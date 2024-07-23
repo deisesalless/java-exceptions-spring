12b4124fd16216c12b0716b68de18ab59e4b3af2
Deise Sales <deisesalles18@gmail.com>
2024-07-22 22:23:01 -0300

    1. rascunho da controller e service

--- events/src/main/java/br/com/deisesales/events/controller/EventController.java
+++ events/src/main/java/br/com/deisesales/events/controller/EventController.java
@@ -1,9 +1,13 @@
 package br.com.deisesales.events.controller;
 
+import br.com.deisesales.events.entity.Event;
 import br.com.deisesales.events.service.EventService;
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
 
 @RestController
 @RequestMapping("/eventos")
@@ -11,4 +15,23 @@ import org.springframework.web.bind.annotation.RestController;
 public class EventController {
 
     private final EventService service;
+
+    public Event createEvent() {
+        // service.createEvent;
+        return null;
+    }
+
+    public List<Event> getAllEvents() {
+        // service.getAllEvents;
+        return null;
+    }
+
+    public List<Event> getUpCommingEvents() {
+        // service.getUpCommingEvents;
+        return null;
+    }
+
+    public ResponseEntity<String> registerParticipant(String eventId) {
+        return null;
+    }
 }
--- events/src/main/java/br/com/deisesales/events/service/EventService.java
+++ events/src/main/java/br/com/deisesales/events/service/EventService.java
@@ -1,9 +1,12 @@
 package br.com.deisesales.events.service;
 
+import br.com.deisesales.events.entity.Event;
 import br.com.deisesales.events.repository.EventRepository;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
 
 @Service
 @RequiredArgsConstructor
@@ -11,4 +14,19 @@ import org.springframework.stereotype.Service;
 public class EventService {
 
     private final EventRepository repository;
+
+    public Event createEvent() {
+        // repository.createEvent;
+        return null;
+    }
+
+    public List<Event> getAllEvents() {
+        // repository.getAllEvents;
+        return null;
+    }
+
+    public List<Event> getUpCommingEvents() {
+        // repository.getUpCommingEvents;
+        return null;
+    }
 }
